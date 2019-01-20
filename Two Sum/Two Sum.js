0ce6da014d2ab7ed1484e28f701f32ce8aa3c073
gleen <gleen.lexry@n2nconnect.com>
2019-01-20 17:23:54 +0800

    improved description of algorithm

--- Two Sum/Two Sum.js
+++ Two Sum/Two Sum.js
@@ -14,3 +14,12 @@ const twoSum = (arr, target) => {
 }
 
 console.log(twoSum([7, 0, -4, 5, 2, 3],5))
+
+// what i can take away from this answer. originally i have implemented the brute force method which has the
+// O(n^2) time complexity. Then after research, i have found there are other methods such as the greedy approach
+// , from what i've seen, solving methods with objects is the fastest apporach. This approach is a O(n)
+
+// break down of problem..
+
+// The map object stores the difference between our target and sum, and set that as key in the obj. the value is the actual
+// is the actual element at that point
